869af0a1eb7f0a20b9552a41e8d8c903ef7c9cac
Stefano Miceli <mic.ninja@gmail.com>
2023-12-05 13:24:45 +0100

    adds comments

--- this.js
+++ this.js
@@ -3,6 +3,9 @@ const person = {
   surname: "Verga",
   skills: ["authoring", "copywriting", "psychology"],
   myMethod: function () {
+    // console.log("Ciao sono " + person.name + " " + person.surname); // la variabile person potrebbe cambiare nel tempo. potrebbe non essere sempre disponibile
+    // potremmo volerci riferire ad un'altra variabile di un oggetto simile che è nato dalla clonazione di questo (quindi avrebbe un nome diverso)
+    // con il THIS risolviamo questo enpass
     console.log("THIS", this);
 
     this.skills.forEach(skill => {
